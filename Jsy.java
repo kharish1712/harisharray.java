64ada08584bdf043e3c4f9f33712f92d678b3bd3
kharish1712 <kharishkumar785@gmail.com>
2026-07-20 02:56:50 -0700

    Create Jsy.java

---
+++ Jsy.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int[]marks={70,60,89,90,89};
+	   String[]subject = {"Tamil","English","Maths","Science","Social"};
+	   for(int i=0;i<marks.length;i++){
+	   System.out.println(subject[i]+"="+marks[i]);
+	   }
+	}
+
+
+}
